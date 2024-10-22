68b1a2252688678adc20c91f76477e5565022597
rcalcagno <49599754+rcalcagno@users.noreply.github.com>
2024-10-22 17:39:39 -0300

    Add files via upload

--- src/src_new_v1.1/main.py
+++ src/src_new_v1.1/main.py
@@ -26,11 +26,29 @@ print(freq())
 
 log_msg = ""
 
-utime.sleep(.5)
+# utime.sleep(.5)
 
 while True:
     
     collect()
-    TS2068_IO()
+
+    try:
+        TS2068_IO()
         
- 
+    except Exception as err:
+        
+        try:
+            close("activity.log")
+        except:
+            pass
+        
+        with open("/activity.log", "a") as log:
+            
+            log_msg += "[" + str(time.ticks_us()) + "] "
+            log_msg += "FATAL ERROR!!!:"
+            
+            log.write(log_msg)
+            sys.print_exception(err, log)
+            
+        break        
+        
